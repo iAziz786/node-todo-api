856978d2652cefa14fb91916ae803f32435f4f84
Mohammmad Aziz <mdaziz067@gmail.com>
2017-06-12 06:06:58 +0530

    Add GET /users/me and authentication token

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 const port = process.env.PORT;
 
@@ -105,6 +106,10 @@ app.post('/users', (req, res) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => res.status(400).send(e));
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
+})
 
 app.listen(port, () => {
 console.log(`Started app on port ${port}`);
